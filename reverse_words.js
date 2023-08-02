dcbc97633981245a8b07a1a174221eb4b7aae6a9
kevin-magu <wanjaukevinmagu@gmail.com>
2023-08-02 12:06:47 +0300

    U

--- reverse_words.js
+++ reverse_words.js
@@ -57,27 +57,28 @@ let r =0;
 while(r<cleanedWords.length){
     if(cleanedWords[0] == ' '){
     cleanedWords.splice(0, 1)
-    }
-
-    for(let i=0; i<cleanedWords.length; i++){
-        if(cleanedWords[i+1] == ' '){
-           splicedMiddleWord =  cleanedWords.splice(0, i+2)
-           finalWords.push(splicedMiddleWord.join(''))
-            break;
+    }else{
+        for(let i=0; i<cleanedWords.length; i++){
+            if(cleanedWords[i+1] == ' '){
+               splicedMiddleWord =  cleanedWords.splice(0, i+2)
+               finalWords.push(splicedMiddleWord.join(''))
+                break;
+            }
         }
     }
-    cleanedWords.splice()
     r ++
 }
 
 // get the last word
 let splicedFinalWord = []
 for(let i = 0; i<cleanedWords.length; i++){
+    if(cleanedWords[i] !== ' ')
     splicedFinalWord.push(cleanedWords[i])
 }
+
 finalWords.push(splicedFinalWord.join(''))
 
 console.log(cleanedWords.join(','))
 console.log(finalWords.join(','))
 }// end of main function
-reverseWords("   hello world       general kevin   mikins")
+reverseWords("   hello world       general    kevin    mikins")
