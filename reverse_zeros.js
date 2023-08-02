aef74117edcd6503dbf0a08eef367a82e11a3b55
kevin-magu <wanjaukevinmagu@gmail.com>
2023-08-02 14:39:33 +0300

    U

---
+++ reverse_zeros.js
@@ -0,0 +1,26 @@
+let moveZeroes = (nums) =>{
+let zerosInNums = 0
+for(let i= 0; i<nums.length; i++){
+    if(nums[i] === 0){
+        zerosInNums ++ 
+    }
+}
+
+console.log(zerosInNums)
+
+//remove zeros
+for(let i =0; i<=zerosInNums; i++){
+    for(let m = 0; m<nums.length; m++){
+        if(nums[m] ===0){
+            console.log(nums[m] + " is a zero")
+            nums.splice(m, 1)
+        }
+    }
+}
+
+console.log(nums)
+
+
+return nums;
+}
+moveZeroes([0,0,1])
