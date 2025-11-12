18a4606953699bad0635d48b34413617c06257df
wanjau kevin <wanjaukevinmagu@gmail.com>
2025-11-12 13:56:10 +0300

    did some hard problems

---
+++ 30-days-of-js/counter.js
@@ -0,0 +1,11 @@
+function createCounter(n) {
+  return function() {
+    return n++;
+  };
+}
+
+const counter = createCounter(10);
+console.log(counter()); 
+console.log(counter()); 
+console.log(counter());
+console.log(counter()); 
